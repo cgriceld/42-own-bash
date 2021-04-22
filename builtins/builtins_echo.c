40ae9d3d1f9b44c4ae83fe94386a92b301ab5873
Shock Brenton <sbrenton@ox-o3.msk.21-school.ru>
2021-04-22 18:27:55 +0300

    echo

--- builtins/builtins_echo.c
+++ builtins/builtins_echo.c
@@ -3,19 +3,22 @@
  int	builtins_echo(t_shell *shell, t_seq *tmp_seq)
 {
 	//	эти переменные будут взяты из парса
-	char *buf;
 	char *param;
 
-	param = NULL;
-//	param = "n";
-	buf = NULL;
-//	buf = "builtins";
-	buf = "/Users/sbrenton";
+	int n;
+	int i;
+	ret_status = 0;
 
-	(void)*shell;
-	if (buf != NULL)
-		printf("%s", buf);
-	if (!param)
+	n = 0;
+	i = 1;
+	param = ft_strtrim(tmp_seq->args[i], "\n");
+	if (ft_strncmp(param, "-n", 3) == 0)
+		n = 1;
+	while(tmp_seq->args[i+n] != NULL)
+	{
+		printf("%s", ft_strtrim(tmp_seq->args[i+n], "\n"));
+	}
+	if (n == 1)
 		printf("\n");
 	return (0);
 }
