60cda4ebdee1a23780d75ae24213d6c5719b02ca
Lesia Kiseleva <lesia.kiseleva@gmail.com>
2021-04-26 21:13:13 +0300

    norma try on export

--- builtins/builtins_export.c
+++ builtins/builtins_export.c
@@ -6,13 +6,24 @@
 /*   By: sbrenton <sbrenton@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 13:39:09 by sbrenton          #+#    #+#             */
-/*   Updated: 2021/04/26 01:08:30 by lesia            ###   ########.fr       */
+/*   Updated: 2021/04/26 21:08:24 by lesia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minibash.h"
 
 // поправить по норме!!!
+
+void print_declations(t_list *key_sort, t_shell *shell)
+{
+	if (ft_strncmp((char *)key_sort->content, "_", 2) != 0)
+	{
+		printf("declare -x %s", (char *)(key_sort->content));
+		if (envp_get_value(shell, key_sort->content) != NULL)
+			printf(" =\"%s\"", envp_get_value(shell, key_sort->content));
+		printf("\n");
+	}
+}
 
 int print_export(t_shell *shell)
 {
@@ -31,30 +42,14 @@ int print_export(t_shell *shell)
 	while (key_sort)
 	{
 		if (((char *)key_sort->content)[0] < 'a' || ((char *)key_sort->content)[0] > 'z')
-		{
-			if (ft_strncmp((char *)key_sort->content, "_", 2) != 0)
-			{
-				printf("declare -x %s", (char *)(key_sort->content));
-				if (envp_get_value(shell, key_sort->content) != NULL)
-					printf(" =\"%s\"", envp_get_value(shell, key_sort->content));
-				printf("\n");
-			}
-		}
+			print_declations(key_sort, shell);
 		key_sort = key_sort->next;
 	}
 	key_sort = sort_start;
 	while (key_sort)
 	{
 		if (((char *)key_sort->content)[0] >= 'a' && ((char *)key_sort->content)[0] <= 'z')
-		{
-			if (ft_strncmp((char *)key_sort->content, "_", 2) != 0)
-			{
-				printf("declare -x %s", (char *)(key_sort->content));
-				if (envp_get_value(shell, key_sort->content) != NULL)
-					printf(" =\"%s\"", envp_get_value(shell, key_sort->content));
-				printf("\n");
-			}
-		}
+			print_declations(key_sort, shell);
 		key_sort = key_sort->next;
 	}
 	ft_lstclear(&sort_start, free);
@@ -82,6 +77,39 @@ void pair_param_value(t_seq *tmp_seq, int i,  char **param, char **value)
 	*param = ft_strdup(tmp_seq->args[i]);
 }
 
+
+int check_is_valid(t_seq *tmp_seq, int i)
+{
+	int n;
+
+	n = 0;
+	if ((tmp_seq->args[i][0] == '_') ||
+		(tmp_seq->args[i][0] >= 'a' && tmp_seq->args[i][0] <= 'z') ||
+		(tmp_seq->args[i][0] >= 'A' && tmp_seq->args[i][0] <= 'Z'))
+		n++;
+	else
+	{
+		printf("export: `%s': not a valid identifier\n", tmp_seq->args[i]);
+		return (1);
+	}
+	while (tmp_seq->args[i][n] != 0 && tmp_seq->args[i][n] != '=')
+	{
+		if ((tmp_seq->args[i][n] == '_') || (tmp_seq->args[i][n] == '\n') ||
+			(tmp_seq->args[i][n] >= 'a' && tmp_seq->args[i][n] <= 'z') ||
+			(tmp_seq->args[i][n] >= 'A' && tmp_seq->args[i][n] <= 'Z') ||
+			(tmp_seq->args[i][n] >= '0' && tmp_seq->args[i][n] <= '9'))
+			n++;
+		else
+		{
+			printf("export: `%s': not a valid identifier\n", tmp_seq->args[i]);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+// умирает прии  export g7 hj k9 . пока хз почему.
+
 int builtins_export(t_shell *shell, t_seq *tmp_seq)
 {
 
@@ -98,12 +126,15 @@ int builtins_export(t_shell *shell, t_seq *tmp_seq)
 	{
 		value = NULL;
 		param = NULL;
-		if (tmp_seq->args[i][0] == '=')
-	//	(tmp_seq->args[i][0] >= 'а' && tmp_seq->args[i][0] <= 'я'))
-		{
-			printf("export: `%s': not a valid identifier\n", tmp_seq->args[i]);
-				return (1);
-		}
+		if (check_is_valid(tmp_seq, i) == 1)
+			return (1);
+//		if ((tmp_seq->args[i][0] == '_') ||
+//		(tmp_seq->args[i][0] >= 'a' && tmp_seq->args[i][0] <= 'z') ||
+//		(tmp_seq->args[i][0] >= 'A' && tmp_seq->args[i][0] <= 'Z'))
+//		{
+//			printf("export: `%s': not a valid identifier\n", tmp_seq->args[i]);
+//				return (1);
+//		}
 		pair_param_value(tmp_seq, i, &param, &value);
 		if (value == NULL && envp_get_value(shell, param) != NULL)
 		{
