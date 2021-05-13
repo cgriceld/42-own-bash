9ce07018d3ee38d8312b732f233927a240d06af1
Lesia Kiseleva <lesia.kiseleva@gmail.com>
2021-05-13 13:25:53 +0300

    stable unset

--- builtins/builtins_unset.c
+++ builtins/builtins_unset.c
@@ -3,62 +3,74 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_unset.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgriceld <cgriceld@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sbrenton <sbrenton@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 13:38:46 by sbrenton          #+#    #+#             */
-/*   Updated: 2021/05/12 12:29:36 by cgriceld         ###   ########.fr       */
+/*   Updated: 2021/05/13 13:23:58 by lesia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../../Downloads/42-own-bash-main/minibash.h"
+#include "../minibash.h"
 
-// int	static first_value(t_shell *shell, t_seq *tmp_seq, char *str_low)
-// {
-// //			this = env;
-// //			shell->env = env->next;
-// //			free(this);
-// //			*i += 1;
+int	static	unset_value( t_shell *shell, t_env	*env, int i)
+{
+	t_env	*this;
 
-// }
+	this = env;
+	shell->env = env->next;
+	free(this->key);
+	free(this->value);
+	this->next = NULL;
+	free(this);
+	i += 1;
+	return (i);
+}
 
+int	static	unset_last( t_env *previous, t_env	*env, int i)
+{
+	t_env	*this;
+
+	this = env;
+	previous->next = env->next;
+	free(this->key);
+	free(this->value);
+	this->next = NULL;
+	free(this);
+	i += 1;
+	return (i);
+}
+
+//убрать 6 строк еще
 int	builtins_unset_value(t_shell *shell, t_seq *tmp_seq, char *str_low)
 {
 	t_env	*env;
 	t_env	*previous;
-	t_env	*this;
-	int		*i;
+	int		i;
 	int		len;
 
 	if (redir(shell, tmp_seq, str_low, 1))
 		return (2);
-	*i = 1;
-	while (tmp_seq->args[*i] != 0)
+	i = 1;
+	while (tmp_seq->args[i] != 0)
 	{
-		len = ft_strlen(tmp_seq->args[*i]);
+		len = ft_strlen(tmp_seq->args[i]);
 		env = shell->env;
-		if (ft_strncmp(env->key, tmp_seq->args[*i], len) == 0)
+		if (ft_strncmp(env->key, tmp_seq->args[i], len) == 0)
 		{
-			this = env;
-			shell->env = env->next;
-			free(this);
-			*i += 1;
-			continue;
+			i = unset_value(shell, env, i);
+			continue ;
 		}
-		while (ft_strncmp(env->key, tmp_seq->args[*i], len) != 0 && env->next)
+		while (ft_strncmp(env->key, tmp_seq->args[i], len) != 0 && env->next)
 		{
 			previous = env;
 			env = env->next;
 		}
-		if (ft_strncmp(env->key, tmp_seq->args[*i], len) != 0)
+		if (ft_strncmp(env->key, tmp_seq->args[i], len) != 0)
 		{
-			*i += 1;
-			continue;
+			i += 1;
+			continue ;
 		}
-		this = env;
-		previous->next = env->next;
-		this->next = NULL;
-		free(this);
-		*i += 1;
+		i = unset_last(previous, env, i);
 	}
 	return (0);
 }
